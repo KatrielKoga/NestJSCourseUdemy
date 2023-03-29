a7ab338a04b2688d0c6ec63996761ea4f547024a
KatrielKoga <katriel.koga.2012@hotmail.com>
2023-03-29 18:39:41 -0300

    start working with cookies

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common/decorators';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
@@ -12,8 +12,8 @@ export default class AuthService {
 
   async signup(email: string, password: string) {
     // See if email is in use
-    const user = await this.userService.find(email);
-    if (user.length) {
+    const users = await this.userService.find(email);
+    if (users.length) {
       new BadRequestException('email in use');
     }
 
@@ -26,10 +26,27 @@ export default class AuthService {
 
     // join the hashed result and the salt together
     const result = salt + '.' + hash.toString('hex');
+
     // creatge anew user and save it
+    const user = await this.userService.create(email, result);
 
     // return the user
+    return user;
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.userService.find(email);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (hash.toString('hex') !== storedHash) {
+      throw new BadRequestException('bad password');
+    }
+
+    return user;
+  }
 }
--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -9,7 +9,9 @@ import {
   Query,
   NotFoundException,
 } from '@nestjs/common';
+import { Session } from '@nestjs/common/decorators';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import AuthService from './auth.service';
 import CreateUserDto from './dtos/create-user.dto';
 import UpdateUserDto from './dtos/update-user.dto';
 import UserDto from './dtos/user.dto';
@@ -18,11 +20,23 @@ import { UsersService } from './users.service';
 @Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.usersService.create(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get('/:id')
